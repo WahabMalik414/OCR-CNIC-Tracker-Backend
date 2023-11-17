73e04a2ccd46836fed04ac52eaea0e40f5848e46
Malik Abdul Wahab <malikwahab34@gmail.com>
2023-11-17 07:17:19 +0500

    view-records-update

--- src/server.ts
+++ src/server.ts
@@ -41,47 +41,104 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+//const upload = multer({ storage: storage });
+const multi_upload = multer({
+  storage,
+  //limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
+  fileFilter: (req: Request, file: any, cb: Function) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      const err = new Error("Only .png, .jpg and .jpeg format allowed!");
+      err.name = "ExtensionError";
+      return cb(err);
+    }
+  },
+}).array("files");
+
+// app.post(
+//   "/process",
+//   //upload.array("files"),
+//   expressAsyncHandler(async (req, res) => {
+//     //await upload.single("files")(req, res);
+
+//     const files = req.files as Express.Multer.File[]; // Files are available here
+
+//     const filenames = files.map((file: any) => file.originalname);
+//     console.log(filenames);
+
+//     const workerInput: WorkerInput = {
+//       inputDir: INPUT_DIR,
+//       outputDir: OUTPUT_DIR,
+//       files: filenames,
+//     };
+
+//     try {
+//       let logs = await processImages(workerInput);
+//       res.status(200).json({ logs });
+//     } catch (error) {
+//       res.status(500).json({ error: "An error occurred" });
+//     }
+//   })
+// );
 
 app.post(
   "/process",
-  upload.array("files"),
   expressAsyncHandler(async (req, res) => {
-    const files = req.files as Express.Multer.File[]; // Files are available here
-
-    const filenames = files.map((file) => file.originalname);
-    console.log(filenames);
-
-    const workerInput: WorkerInput = {
-      inputDir: INPUT_DIR,
-      outputDir: OUTPUT_DIR,
-      files: filenames,
-    };
+    const uploadPromise = new Promise((resolve, reject) => {
+      multi_upload(req, res, function (err: any) {
+        if (err instanceof multer.MulterError) {
+          reject({ message: `Multer uploading error: ${err.message}` });
+        } else if (err) {
+          if (err.name == "ExtensionError") {
+            reject({ message: err.message });
+          } else {
+            reject({ message: `Unknown uploading error: ${err.message}` });
+          }
+        } else {
+          resolve("Your files uploaded.");
+        }
+      });
+    });
 
     try {
-      let logs = await processImages(workerInput);
-      res.status(200).json({ logs });
+      const result = await uploadPromise;
+      console.log(result);
+      const files = req.files as Express.Multer.File[];
+      const filenames = files.map((file: any) => file.originalname);
+      console.log(filenames);
+      const workerInput: WorkerInput = {
+        inputDir: INPUT_DIR,
+        outputDir: OUTPUT_DIR,
+        files: filenames,
+      };
+      try {
+        let logs = await processImages(workerInput);
+        res.status(200).json({ logs });
+      } catch (error) {
+        res.status(500).json({ error: "An error occurred" });
+      }
     } catch (error) {
-      res.status(500).json({ error: "An error occurred" });
+      res.status(500).send({ error });
     }
   })
 );
 
-app.get(
-  "/view",async(req:Request,res:Response)=>{
-    console.log("request arrived!")
-    try {
-      const allRecords = await db.data.findMany();
-      console.log(allRecords);
-      res.status(200).json(allRecords);
-    } catch (error) {
-      res.status(404).json(error)
-    }
-   
+app.get("/view", async (req: Request, res: Response) => {
+  console.log("request arrived!");
+  try {
+    const allRecords = await db.data.findMany();
+    console.log(allRecords);
+    res.status(200).json(allRecords);
+  } catch (error) {
+    res.status(404).json(error);
   }
-  
-);
-
+});
 
 app.listen(port, () => {
   console.log(`[Server]: I am running at https://localhost:${port}`);
