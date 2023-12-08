94272d9412d932bebd815fbaf535bc49fd80ec99
WahabMalik414 <malikwahab34@gmail.com>
2023-12-08 11:07:09 +0500

    multer fixed

--- src/server.ts
+++ src/server.ts
@@ -39,135 +39,39 @@ const storage = multer.diskStorage({
   ) => {
     const originalName = file.originalname;
     callback(null, originalName);
-
-    //const ext = path.extname(originalName);
-    //callback(null, originalName + ext);
   },
 });
 
-//const upload = multer({ storage: storage });
-const multi_upload = multer({
-  storage,
-  //limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
-  fileFilter: (req: Request, file: any, cb: Function) => {
-    if (
-      file.mimetype == "image/png" ||
-      file.mimetype == "image/jpg" ||
-      file.mimetype == "image/jpeg"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-      const err = new Error("Only .png, .jpg and .jpeg format allowed!");
-      err.name = "ExtensionError";
-      return cb(err);
-    }
-  },
-}).array("files");
+const upload = multer({ storage: storage });
 
-const multi_upload2 = multer({
-  storage,
-  fileFilter: (req:Request, file:any, cb:Function) => {
-    const ext = path.extname(file.originalName).toLowerCase();
-    const mimetype = file.mimetype;
-    if (
-      ext !== '.png' && 
-      ext !== '.jpg' && 
-      ext !== '.jpeg' && 
-      mimetype !== 'image/png' && 
-      mimetype !== 'image/jpg' && 
-      mimetype !== 'image/jpeg'
-    ) {
-      const err = new Error("Only .png, .jpg and .jpeg format allowed!");
-      err.name = "ExtensionError";
-      return cb(err);
-    }
-    cb(null, true);
-  },
- }).array("files");
-// app.post(
-//   "/process",
-//   //upload.array("files"),
-//   expressAsyncHandler(async (req, res) => {
-//     //await upload.single("files")(req, res);
-
-//     const files = req.files as Express.Multer.File[]; // Files are available here
-
-//     const filenames = files.map((file: any) => file.originalname);
-//     console.log(filenames);
-
-//     const workerInput: WorkerInput = {
-//       inputDir: INPUT_DIR,
-//       outputDir: OUTPUT_DIR,
-//       files: filenames,
-//     };
-
-//     try {
-//       let logs = await processImages(workerInput);
-//       res.status(200).json({ logs });
-//     } catch (error) {
-//       res.status(500).json({ error: "An error occurred" });
-//     }
-//   })
-// );
-// Define the path to the "input" folder
-const inputFolderPath = path.resolve(__dirname, '../input');
-
-// Serve a specific file based on the request
-app.get('/api/files/:fileName', (req, res) => {
-  const fileName = req.params.fileName;
-  const filePath = path.join(inputFolderPath, fileName);
-  console.log(filePath)
-
-  // Use res.sendFile to send the file
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      // Handle errors (e.g., file not found)
-      res.status(404).send('File not found');
-    }
-  });
-});
 app.post(
   "/process",
-  expressAsyncHandler(async (req, res) => {
-    const uploadPromise = new Promise((resolve, reject) => {
-      multi_upload(req, res, function (err: any) {
-        if (err) {
-          reject({ message: `Multer uploading error: ${err.message}` });
-        } else if (err) {
-          if (err.name == "ExtensionError") {
-            reject({ message: err.message });
-          } else {
-            reject({ message: `Unknown uploading error: ${err.message}` });
-          }
-        } else {
-          resolve("Your files uploaded.");
-        }
-      });
-    });
+  upload.array("files"),
+  async (req, res) => {
+    await upload.single("files");
+
+    const files = req.files as Express.Multer.File[]; // Files are available here
+
+    const filenames = files.map((file: any) => file.originalname);
+    console.log(filenames);
+
+    const workerInput: WorkerInput = {
+      inputDir: INPUT_DIR,
+      outputDir: OUTPUT_DIR,
+      files: filenames,
+    };
 
     try {
-      const result = await uploadPromise;
-      console.log(result);
-      const files = req.files as Express.Multer.File[];
-      const filenames = files.map((file: any) => file.originalname);
-      console.log(filenames);
-      const workerInput: WorkerInput = {
-        inputDir: INPUT_DIR,
-        outputDir: OUTPUT_DIR,
-        files: filenames,
-      };
-      try {
-        let logs = await processImages(workerInput);
-        res.status(200).json({ logs });
-      } catch (error) {
-        res.status(500).json({ error: "An error occurred" });
-      }
+      let logs = await processImages(workerInput);
+      res.status(200).json({ logs });
     } catch (error) {
-      res.status(500).send({ error });
+      res.status(500).json({ error: "An error occurred" });
     }
-  })
+  }
 );
+
+
+
 
 app.get("/view", async (req: Request, res: Response) => {
   try {
